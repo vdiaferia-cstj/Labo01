6c9aa111f802d5242df89ec9fe72034c3a5738ff
Vincent <1967480@cstj.qc.ca>
2020-09-03 12:02:32 -0400

    Tout roule parfaitement. Juste un problème d'accent encore.

---
+++ Projet/test.cpp
@@ -0,0 +1,14 @@
+// But : Utiliser git et GitHub en affichant un message à l'écran
+// Auteur : Vincent Diaferia
+// Date : 3 septembre 2002
+
+#include <iostream> // Pour permettre au programme d'intéragir avec l'utilisateur cout,cin
+
+int main()
+{
+
+	//Afficher un message à l'écran
+	std::cout << "Bienvenue à tous!";
+
+	return 0;
+}
